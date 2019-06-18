3c300b5c8323bcfdd5a54b940135b73d52a12a2c
Andrew Sprague <asprague@nephilaadvisors.com>
2019-06-17 17:14:47 -0700

    Add rstudio install (experimental)

---
+++ src/databricksruntime-rbase-rstudio/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+if [[ $DB_IS_DRIVER = "TRUE" ]]; then
+    echo "Starting the rstudio daemon"
+    rstudio-server restart
+else
+    echo "Not starting the rstudio daemon (this is not the spark driver)"
+fi
